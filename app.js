9a674e1307ece26e5c39f54767145133dccf6748
Judongjae <wnehdwo0982@naver.com>
2022-03-27 05:03:40 +0900

    라우트 최적화

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
+"use strict";
+
+//모듈
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="ko">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    여기는 루트입니다.
-  </body>
-  </html>
-`);
-});
+const PORT = 3000;
 
-app.get("/login", (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="ko">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
-    </head>
-    <body>
-      <input type="text" placeholder='아이디'><br>
-      <input type="text" placeholder='비밀번호'><br>
-      <button>로그인</button>
-    </body>
-    </html>
-  `);
-});
+//라우팅
+const home = require("./routes/home");
 
-app.listen(3000, () => {
+//앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
+
+app.listen(PORT, () => {
   console.log("서버가동");
 });
