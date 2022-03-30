7c2bc3d909178a4ec33e3a954f6fa1d3441237b1
Judongjae <wnehdwo0982@naver.com>
2022-03-31 07:47:20 +0900

    프런트 파일만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views"); //view 엔진 경로
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
 
