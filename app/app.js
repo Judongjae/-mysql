9e1ca6926cb8ce60af82295e51fb8978cabf4cec
Judongjae <wnehdwo0982@naver.com>
2022-03-31 07:36:03 +0900

    파일위치 변경 및 오류해결

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//모듈
+const express = require("express");
+const app = express();
+
+//라우팅
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views"); //view 엔진 경로
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
+
+module.exports = app;
