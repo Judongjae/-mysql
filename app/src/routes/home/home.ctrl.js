f6f198c6794962a6b24992823c06839c535e63a8
Judongjae <wnehdwo0982@naver.com>
2022-04-02 22:18:54 +0900

    로그인기능구현1

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -20,7 +20,20 @@ const process = {
     const id = req.body.id,
       psword = req.body.psword;
 
-    console.log(id, psword);
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        //1.로그인이 성공하면
+        return res.json({
+          //3.json으로 만들어서 res, 프론트엔드로 응답을 해준다.
+          success: true, //2.success: true라는 오브젝트를
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
