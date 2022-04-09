baabda50575fc1a0ca1dcdf59b004bdac17eb141
Judongjae <wnehdwo0982@naver.com>
2022-04-09 12:13:40 +0900

    DB파일로 구현중

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,10 @@
 "use strict";
 
+const fs = require("fs");
+
 class UserStorage {
-  static #users = {
-    //데이터 은닉화 외부에서 불러올 수 없음
-    id: ["주동재", "wnehdwo", "박준영"],
-    psword: ["1234", "0626", "1234"],
-    name: ["wnehdwo", "김팀장", "주차장"],
-  };
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -19,19 +15,22 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users);
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
+    fs.readFile("./src/databases/users.json", (err, data) => {
+      if (err) throw err;
+      const users = JSON.parse(data);
+      const idx = users.id.indexOf(id);
+      const usersKeys = Object.keys(users);
+      const userInfo = usersKeys.reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+      }, {});
 
-    return userInfo;
+      return userInfo;
+    });
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
